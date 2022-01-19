cc92eab8b8ebc3eed0684d454d5203caeba10c7e
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-19 22:52:26 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,55 +1,55 @@
 #include "middle_list.h"
 
-vector<char> itc_strtlist(string str) {
+vector<char> itc_strtlist(string s) {
     vector<char> ans;
-    for (unsigned int i = 0; str[i] != '\0'; i++) ans.push_back(str[i]);
+    for (unsigned int i = 0; s[i] != '\0'; i++) ans.push_back(s[i]);
     return ans;
 }
 
-string itc_join(vector <char> lst, string sep) {
+string itc_join(vector <char> list, string sep) {
     string ans = "";
-    if (lst.size() > 0) {
-        for (int i = 0; i < lst.size() - 1; i++) {
-            ans += lst[i];
+    if (list.size() > 0) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            ans += list[i];
             ans += sep;
         }
-        ans += lst[lst.size() - 1];
+        ans += list[list.size() - 1];
     }
     return ans;
 }
 
-string itc_rmstrspc(string str) {
+string itc_rmstrspc(string s) {
     string ans = "";
-    for (int i = 0; str[i] != '\0'; i++) if (str[i] != ' ') ans += str[i];
+    for (int i = 0; s[i] != '\0'; i++) if (s[i] != ' ') ans += s[i];
     return ans;
 }
 
-unsigned long long itc_len(string str) {
+unsigned long long itc_len(string s) {
     unsigned long long a = 0;
-    while (str[a] != '\0') a++;
+    while (s[a] != '\0') a++;
     return a;
 }
 
-int itc_find_str(string str1, string str2) {
-    if (itc_len(str2) > itc_len(str1))
+int itc_find_str(string s1, string s2) {
+    if (itc_len(s2) > itc_len(s1))
         return -1;
     else
     {
         int answer = 1;
-        for (int i = 0; i < itc_len(str1); i++)
+        for (int i = 0; i < itc_len(s1); i++)
         {
-            if (str1[i] == str2[0] && itc_len(str1) - i >= itc_len(str2))
+            if (s1[i] == s2[0] && itc_len(s1) - i >= itc_len(s2))
             {
-                for (int j = 1; j < itc_len(str2); j++)
+                for (int j = 1; j < itc_len(s2); j++)
                 {
-                    if (str2[j] == str1[i + j])
+                    if (s2[j] == s1[i + j])
                         answer++;
                     else
                     {
                         answer = 1;
                     }
                 }
-                if (answer == itc_len(str2))
+                if (answer == itc_len(s2))
                     return i;
             }
         }
@@ -57,51 +57,51 @@ int itc_find_str(string str1, string str2) {
     }
 }
 
-long long itc_sumlst(const vector <int> &lst){
+long long itc_sumlst(const vector <int> &list){
     
     long long sum = 0;
-    for(int i = 0; i < lst.size(); i++ ){
-        sum += lst[i];
+    for(int i = 0; i < list.size(); i++ ){
+        sum += list[i];
     }
     return sum;
 }
-long long itc_sum_even_lst(const vector<int>& lst) {
+long long itc_sum_even_lst(const vector<int>& list) {
     long long sum = 0;
-    for (int i = 1; i < lst.size(); i += 2) {
-        sum += lst[i];
+    for (int i = 1; i < list.size(); i += 2) {
+        sum += list[i];
     }
     return sum;
 }
 
-long long itc_sum_even_part_lst(const vector<int>& lst) {
+long long itc_sum_even_part_lst(const vector<int>& list) {
     long long sum = 0;
-    for (int i = 0; i < lst.size(); i++) {
+    for (int i = 0; i < list.size(); i++) {
         if (lst[i] % 2 == 0)
-            sum += lst[i];
+            sum += list[i];
     }
     return sum;
 }
 
-void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2)
+void itc_odd_even_separator_lst(const vector <int> &list, vector <int> &list1, vector <int> &list2)
 {
-    for(long long i =0; i < lst.size(); i++){
-        if(lst[i] % 2 == 0){
-            lst1.push_back(lst[i]);
+    for(long long i =0; i < list.size(); i++){
+        if(list[i] % 2 == 0){
+            list1.push_back(list[i]);
         }else{
-            lst2.push_back(lst[i]);
+            list2.push_back(list[i]);
         }
     }
 }
 
-void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3)
+void itc_pos_neg_separator_lst(const vector <int> &list, vector <int> &list1, vector <int> &list2, vector <int> &list3)
 {
-    for(long long i =0; i < lst.size(); i++){
-        if (lst[i] < 0){
-            lst1.push_back(lst[i]);
+    for(long long i =0; i < list.size(); i++){
+        if (list[i] < 0){
+            list1.push_back(list[i]);
         }else if (lst[i] == 0){
-            lst2.push_back(lst[i]);
+            list2.push_back(list[i]);
         }else{
-            lst3.push_back(lst[i]);
+            list3.push_back(list[i]);
         }
     }
 }
