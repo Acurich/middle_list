662bafa88c23df14ef90e7433eb941ce17f46c34
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-20 13:34:26 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -100,102 +100,50 @@ void itc_pos_neg_separator_lst(const vector <int> &list, vector <int> &list1, ve
             list3.push_back(list[i]);
     }
 }
-void itc_odd_even_analysis_lst(const vector<int> &list){
-    setlocale( LC_ALL,"Russian" );
-    int cntlist =  list.size();
-    vector<int> cnt;
-    vector<int> maxcnt;
-    vector<int> mincnt;
-    vector<long> sum;
-    vector<bool> empty;
-    for(int i =0; i < 2; i++  ){
-        cnt.push_back(0);
-        maxcnt.push_back(0);
-        mincnt.push_back(0);
-        sum.push_back(0);
-        empty.push_back(false);
-    }
-    for(int i = 0 ; i < cntlist ; i++){
-        int h;
-        if( list[i] % 2 == 0){
-        h = 0;
-        }else{
-        h = 1;
+int itc_max_lst(const vector <int> &lst){
+    int max_num = -2147483648;
+    if (lst.size() > 0){
+        for(int i =0; i < lst.size(); i++){
+            if (lst[i] > max_num)
+                max_num = lst[i];
         }
-        ++cnt[h];
-        if( empty[h] == false ) {
-            empty[h] = true;
-            maxcnt[h] = list[i];
-            mincnt[h] = list[i];
-        } else{
-            if( list[i] > maxcnt[h]){
-                maxcnt[h] = list[i];
-            }
-            if( list[i] < mincnt[h]){
-                mincnt[h] = list[i];
-            }
-        }
-            sum[h]+=list[i];
-
     }
-    cout<<"Àíàëèç ñïèñêà:"<<endl
-        <<"Êîëè÷åñòâî ÷åòíûõ ÷èñåë: "<<cnt[0]<<","<<"		"<<"Êîëè÷åñòâî íå÷åòíûõ ÷èñåë: "<<cnt[1]<<","<<endl
-        <<"Ìàêñèìàëüíàÿ ÷åòíàÿ öèôðà: "<<maxcnt[0]<<","<<"		"<<"Ìàêñèìàëüíàÿ íå÷åòíàÿ öèôðà: "<<maxcnt[1]<<","<<endl
-        <<"Ìèíèìàëüíàÿ ÷åòíàÿ öèôðà: "<<mincnt[0]<<","<<"		"<<"Ìèíèìàëüíàÿ íå÷åòíàÿ öèôðà: "<<mincnt[1]<<","<<endl
-        <<"Ñóììà ÷åòíûõ ÷èñåë: "<<sum[0]<<","<<"			"<<"Ñóììà íå÷åòíûõ ÷èñåë: "<<sum[1]<<","<<endl;
+    return max_num;
 }
-void itc_pos_neg_analysis_lst(const vector <int> &list){
-    setlocale( LC_ALL,"Russian" );
-    int cntlist =  list.size();
-    vector<int> cnt;
-    vector<int> maxcnt;
-    vector<int> mincnt;
-    vector<long> sum;
-    vector<bool> empty;
-    int zerocnt = 0;
-    for(int i =0; i < 2; i++  ){
-        cnt.push_back(0);
-        maxcnt.push_back(0);
-        mincnt.push_back(0);
-        sum.push_back(0);
-        empty.push_back(false);
-    }
-    for( int i = 0 ; i < cntlst; ++i ){
-    if( list[i] == 0 ){
-        zerocnt++;
-    } else{
-        int h = 0;
-        if(list[i] > 0){
-            h = 1;
-        }
-        ++cnt[h];
-        if( empty[h] == false ) {
-            empty[h] = true;
-            maxcnt[h] = list[i];
-            mincnt[h] = list[i];
-        } else{
-            if( lst[i] > maxcnt[h]){
-                maxcnt[h] = list[i];
-            }
-            if( list[i] < mincnt[h]){
-                mincnt[h] = list[i];
-            }
-        }
-            sum[h]+=list[i];
 
+int itc_min_lst(const vector <int> &lst){
+    int min_num = 2147483648;
+    if (lst.size() > 0){
+        for(int i =0; i < lst.size(); i++){
+            if (lst[i] < min_num)
+                min_num = lst[i];
+        }
     }
+    return min_num;
 
 }
-    vector <double> middle;
-    for(int i =0; i < 2; i++  ){
-        middle.push_back((double)(sum[i])/cnt[i] );
-    }
-    cout<<"Ïîëîæèòåëüíûå:			Îòðèöàòåëüíûå:"<<endl
-    <<"Êîëè÷åñòâî ÷èñåë: "<<cnt[1]<<","<<"	"<<"Êîëè÷åñòâî ÷èñåë: "<<cnt[0]<<","<<endl
-    <<"Ìàêñèìàëüíàÿ öèôðà: "<<maxcnt[1]<<","<<"		"<<"Ìàêñèìàëüíàÿ öèôðà: "<<maxcnt[0]<<","<<endl
-    <<"Ìèíèìàëüíàÿ öèôðà: "<<mincnt[1]<<","<<"		"<<"Ìèíèìàëüíàÿ öèôðà: "<<mincnt[0]<<","<<endl
-    <<"Ñóììà ÷èñåë: "<<sum[1]<<","<<"			"<<"Ñóììà ÷èñåë: "<<sum[0]<<","<<endl
-    <<"Ñðåäíåå çíà÷åíèå: "<<middle[1]<<" 		"<<"Ñðåäíåå çíà÷åíèå: "<<middle[0]<<endl
-    <<" "<<endl
-    <<"Êîëè÷åñòâî íóëåé: "<<zerocnt<<endl;
+
+
+void itc_odd_even_analysis_lst(const vector <int> &list){
+    setlocale(LC_ALL, "rus");
+    vector<int> list1, list2;
+    itc_odd_even_separator_lst(list, list1, list2);
+    cout << "Àíàëèç ñïèñêà:" << endl;
+    cout << "Êîëè÷åñòâî ÷åòíûõ ÷èñåë: " << list1.size() << "," << '\t'    << '\t' << "Êîëè÷åñòâî íå÷åòíûõ ÷èñåë: " << list2.size() << endl;
+    cout << "Ìàêñèìàëüíàÿ ÷åòíàÿ öèôðà: " << itc_max_lst(list1) << "," << '\t' << '\t' << "Ìàêñèìàëüíàÿ íå÷åòíàÿ öèôðà: " << itc_max_lst(list2) <<"," << endl;
+    cout << "Ìèíèìàëüíàÿ ÷åòíàÿ öèôðà: " << itc_min_lst(list1) << "," << '\t' <<'\t' << "Ìèíèìàëüíàÿ íå÷åòíàÿ öèôðà: " << itc_min_lst(list2)<<"," << endl;
+    cout << "Ñóììà ÷åòíûõ ÷èñåë: " << itc_sumlst(list1) << "," << '\t' <<'\t' << "Ñóììà íå÷åòíûõ ÷èñåë: "  << itc_sumlst(list2)<<"," << endl;
+}
+ void itc_pos_neg_analysis_lst(const vector <int> &list){
+    setlocale(LC_ALL, "rus");
+    vector <int> negge, zero, positive;
+    itc_pos_neg_separator_lst(list, negge, zero, positive);
+    cout << "Положительные:" << '\t' << '\t' << "Отрицательные:" << endl;
+    cout << "Количество чисел: " << positive.size() << "," << '\t' << '\t' << "Количество чисел: " << negge.size() << "," << endl;
+    cout << "Максимальная цифра: " << itc_max_lst(positive) << "," << '\t' << '\t' << "Максимальная цифра: " << itc_max_lst(negge) <<  "," << endl;
+    cout << "Минимальная цифра: " << itc_min_lst(positive) << "," << '\t' << '\t' << "Минимальная цифра: " << itc_min_lst(negge) << "," << endl;
+    cout << "Сумма чисел: "<< itc_sumlst(positive) << "," << '\t'	<< '\t' << "Сумма чисел: "<< itc_sumlst(negge) <<"," << endl;
+    cout << "Среднее значение: "<< itc_sumlst(positive) / positive.size()<< '\t'	<< '\t' << "Среднее значение: " << itc_sumlst(negge) / negge.size() << endl;
+cout << endl;
+   cout << "Количество нулей: " << zero.size() << endl;
 }
