314f7dc725737e24e1d93cd2c96f387f50d1ab4b
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-20 17:50:01 +0300

    Update middle_list.h

--- middle_list.h
+++ middle_list.h
@@ -4,19 +4,18 @@
 #include<vector>
 #include<iostream>
 using namespace std;
-int itc_min_lst(const vector <int> &lst);
-int itc_max_lst(const vector <int> &list);
-int len(string str);
-vector <char> itc_strtlist(string s);
-string itc_join(vector <char> list , string sep);
+int len(string s);
+vector<char> itc_strtlist(string s);
+string itc_join(vector <char> list, string sep);
 string itc_rmstrspc(string s);
-string itc_rmstrchar(string s,string less);
-long itc_sumlst(const vector<int> &list);
-long itc_sum_even_lst(const vector<int> &list);
-long itc_sum_even_part_lst(const vector<int> &list);
-void itc_odd_even_separator_lst(const vector<int> &list,vector<int> &list1,vector<int> &list2);
-void itc_pos_neg_separator_lst(const vector<int> &list,vector<int> &list1,vector<int> &list2,vector<int> &list3);
-void itc_odd_even_analysis_lst(const vector<int> &list);
+string itc_rmstrchar(string s, string less);
+long itc_sumlst(const vector <int> &list);
+long itc_sum_even_lst(const vector <int> &list);
+long itc_sum_even_part_lst(const vector <int> &list);
+void itc_odd_even_separator_lst(const vector <int> &list, vector <int> &list1, vector <int> &list2);
+void itc_pos_neg_separator_lst(const vector <int> &list, vector <int> &list1, vector <int> &list2, vector <int> &list3);
+int itc_max_lst(const vector <int> &list);
+int itc_min_lst(const vector <int> &list);
+void itc_odd_even_analysis_lst(const vector <int> &list);
 void itc_pos_neg_analysis_lst(const vector <int> &list);
-
 #endif // EASY_LIST_H_INCLUDED
