b73cd2102233099484a37bea61939c09a7edafc7
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-20 09:30:37 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -119,19 +119,35 @@ void itc_pos_neg_separator_lst(const vector <int> &list, vector <int> &list1, ve
         }
     }
 }
-    void itc_odd_even_analysis_lst(const vector<int> &lst){
-    if(lst.size() == 0)
+    void itc_odd_even_analysis_lst(const vector<int> &list){
+    if(list.size() == 0)
         return;
     long long buffer1 = 0; //even numbers number
     long long evenSum = 0;
     long long buffer2 = 0; //odd numbers number
     long long oddSum = 0;
-    for(long long i = 0; i < lst.size(); i++){
-        lst[i] % 2 == 0 ? buffer1++ : buffer2++;
-        lst[i] % 2 == 0 ? evenSum += lst[i] : oddSum += lst[i];
+    for(long long i = 0; i < list.size(); i++){
+        list[i] % 2 == 0 ? buffer1++ : buffer2++;
+        list[i] % 2 == 0 ? evenSum += list[i] : oddSum += list[i];
     }
     cout << "Количество четных чисел: " << buffer1 << ",        Количество нечетных чисел: " << buffer2 << endl;
-    cout << "Максимальная четная цифра: " << itc_max_vector_num(lst, true) << ",        Максимальная нечетная цифра: " << itc_max_vector_num(lst, false) << "," << endl;
-    cout << "Минимальная четная цифра: " << itc_min_vector_num(lst, true) << ",        Минимальная нечетная цифра: " << itc_min_vector_num(lst, false) << "," << endl;
+    cout << "Максимальная четная цифра: " << itc_max_vector_num(list, true) << ",        Максимальная нечетная цифра: " << itc_max_vector_num(list, false) << "," << endl;
+    cout << "Минимальная четная цифра: " << itc_min_vector_num(list, true) << ",        Минимальная нечетная цифра: " << itc_min_vector_num(list, false) << "," << endl;
     cout << "Сумма четных чисел: " << evenSum << ",        Сумма нечетных чисел: " << oddSum << ",";
+}
+void itc_pos_neg_analysis_lst(const vector <int> &lst){
+    setlocale(LC_ALL, "rus"); // language
+    vector <int> ot;
+    vector <int> nul;
+    vector <int> pol;
+    itc_pos_neg_separator_lst(lst, ot, nul, pol);
+    int s = itc_sumlst(ot);
+    int kol = ot.size();
+    cout << "Положительные:"<< '\t'	<< '\t' << "Отрицательные:" << endl;
+    cout << "Количество чисел: " << pol.size() << "," << '\t'	<< '\t' << "Количество чисел: " << ot.size() << "," << endl;
+    cout << "Максимальная цифра: " << itc_max_lst(pol) << "," << '\t'	<< '\t' << "Максимальная цифра: " << itc_max_lst(ot) << "," << endl;
+    cout << "Минимальная цифра: "<< itc_min_lst(pol) << "," << '\t' << '\t' << "Минимальная цифра: "<< itc_min_lst(ot) << "," << endl;
+    cout << "Сумма чисел: "<< itc_sumlst(pol) << "," << '\t'	<< '\t' << "Сумма чисел: "<< itc_sumlst(ot) << "," << endl;
+    cout << "Среднее значение: "<< itc_sumlst(pol) / pol.size()<< '\t'	<< '\t' << "Среднее значение: "<< s / kol << endl;
+    cout << "Количество нулей: " << nul.size() << endl;
 }
