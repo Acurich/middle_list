3c50ebfc7bbf46f59fa1fbf91c284652c3816bbf
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-20 17:41:43 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,24 +1,21 @@
 #include "middle_list.h"
-int len(string str)
+int len(string s)
 {
-    int i = 0;
-    while (str[i] != '\0')
-        i++;
-    return i;
+    int g = 0;
+    while (s[g] != '\0')
+        g++;
+    return g;
 }
 vector<char> itc_strtlist(string s) {
-    vector<char> s;
-    for (unsigned int i = 0; s[i] != '\0'; i++) res.push_back(s[i]);
+    vector<char> res;
+    for (unsigned int g = 0; s[g] != '\0'; g++) res.push_back(s[g]);
     return res;
 }
 
 string itc_join(vector <char> list, string sep) {
     string res = "";
     if (list.size() > 0) {
-        for (int i = 0; i < list.size() - 1; i++) {
-            res += list[i];
-            res += sep;
-        }
+        for (int g = 0; g < list.size() - 1; g++) { res += list[g]; res += sep;}
         res += list[list.size() - 1];
     }
     return res;
@@ -26,103 +23,62 @@ string itc_join(vector <char> list, string sep) {
 string itc_rmstrspc(string s)
 {
     string res = "";
-    for (int i = 0; i < len(s); i++)
-    {
-        if (s[i] != ' ')
-            res += s[i];
-    }
+    for (int g = 0; g < len(s); g++){if (s[g] != ' ') res += s[g];}
     return res;
 }
 
 string itc_rmstrchar(string s, string less)
 {
     string res = "";
-    for (int i = 0; i < len(s); i++)
+    for (int g = 0; g < len(s); g++)
     {
         bool flag = false;
-        for (int j = 0; j < len(less); j++)
-        {
-            if (s[i] == less[j])
-                flag = true;
-        }
-        if (!flag)
-            res += s[i];
+        for (int f = 0; f < len(less); f++){if (s[g] == less[f]) flag = true;}
+        if (!flag) res += s[g];
     }
     return res;
 }
 long itc_sumlst(const vector <int> &list){
-    int count = list.size();
-    long result = 0;
-    if(count != 0){
-        for(int i = 0; i < count; i++){
-            result += list[i];
-        }
+    int cnt = list.size();
+    long long res = 0;
+    if(cnt != 0){
+        for(int g = 0; g < cnt; g++){res += list[i];}
     }
-    return result;
+    return res;
 }
-long itc_sum_even_lst(const vector <int> &list){
-    int count = list.size();
-    long result = 0;
-    if(count != 0){
-        for(int i = 0; i < count; i += 2){
-            result += list[i];
-        }
-    }
-    return result;
+long itc_sum_even_lst(const vector <int> &list) {
+    long long res = 0;
+    int cnt = list.size();
+    for (int g = 0; i < cnt; g++) if (g % 2 == 0) res += lst[g];
+    return res;
 }
 long itc_sum_even_part_lst(const vector <int> &list){
-    int count = list.size();
-    long result = 0;
-    if(count != 0){
-        for(int i = 0; i < count; i++){
-            if(list[i] % 2 == 0){
-                result += list[i];
-            }
-        }
-    }
-    return result;
+    int cnt = list.size();
+    long res = 0;
+    if(cnt != 0){for(int g = 0; g < cnt; g++){ if(list[g] % 2 == 0){ res += list[g];}}}
+    return res;
 }
 void itc_odd_even_separator_lst(const vector <int> &list, vector <int> &list1, vector <int> &list2){
-    for(int i = 0; i < list.size(); i++){
-        if(list[i] % 2 == 0)
-            list1.push_back(list[i]);
-        else
-            list2.push_back(list[i]);
-    }
+    int cnt = list.size();
+    for(int g = 0; g < cnt; g++){ if(list[g] % 2 == 0) list1.push_back(list[g]); else list2.push_back(list[g]);}
 }
 void itc_pos_neg_separator_lst(const vector <int> &list, vector <int> &list1, vector <int> &list2, vector <int> &list3){
-    for(int i = 0; i < list.size(); i++){
-        if(list[i] < 0)
-            list1.push_back(list[i]);
-        if(list[i] == 0)
-            list2.push_back(list[i]);
-        if(list[i] > 0)
-            list3.push_back(list[i]);
-    }
+    int cnt = list.size();
+    for(int g = 0; g < cnt; g++){if(list[g] < 0) list1.push_back(list[g]); else if(list[g] == 0) list2.push_back(list[g]); else if(list[g] > 0) list3.push_back(list[g]);}
 }
 int itc_max_lst(const vector <int> &list){
-    int max_num = -2147483648;
-    if (list.size() > 0){
-        for(int i =0; i < list.size(); i++){
-            if (list[i] > max_num)
-                max_num = list[i];
-        }
-    }
-    return max_num;
+    int min_num = -229742966;
+    int cnt = list.size();
+    if (cnt > 0){ for(int g = 0; g < cnt; g++){ if (list[g] > min_num) min_num = list[g];}}
+    return min_num;
 }
 
 int itc_min_lst(const vector <int> &lst){
-    int min_num = 2147483648;
-    if (list.size() > 0){
-        for(int i =0; i < list.size(); i++){
-            if (list[i] < min_num)
-                min_num = list[i];
-        }
-    }
-    return min_num;
-
+    int max_num = 229742966;
+    int cnt = list.size();
+    if (cnt > 0){ for(int g = 0; g < cnt; g++){ if (list[g] < max_num) max_num = list[g];}}
+    return max_num;
 }
-
 
 void itc_odd_even_analysis_lst(const vector <int> &list){
     setlocale(LC_ALL, "rus");
@@ -136,14 +92,14 @@ void itc_odd_even_analysis_lst(const vector <int> &list){
 }
  void itc_pos_neg_analysis_lst(const vector <int> &list){
     setlocale(LC_ALL, "rus");
-    vector <int> negge, zero, positive;
-    itc_pos_neg_separator_lst(list, negge, zero, positive);
+    vector <int> pos, null, kek;
+    itc_pos_neg_separator_lst(list, pos, null, kek);
     cout << "Положительные:" << '\t' << '\t' << "Отрицательные:" << endl;
-    cout << "Количество чисел: " << positive.size() << "," << '\t' << '\t' << "Количество чисел: " << negge.size() << "," << endl;
-    cout << "Максимальная цифра: " << itc_max_lst(positive) << "," << '\t' << '\t' << "Максимальная цифра: " << itc_max_lst(negge) <<  "," << endl;
-    cout << "Минимальная цифра: " << itc_min_lst(positive) << "," << '\t' << '\t' << "Минимальная цифра: " << itc_min_lst(negge) << "," << endl;
-    cout << "Сумма чисел: "<< itc_sumlst(positive) << "," << '\t'	<< '\t' << "Сумма чисел: "<< itc_sumlst(negge) <<"," << endl;
-    cout << "Среднее значение: "<< itc_sumlst(positive) / positive.size()<< '\t'	<< '\t' << "Среднее значение: " << itc_sumlst(negge) / negge.size() << endl;
-cout << endl;
-   cout << "Количество нулей: " << zero.size() << endl;
+    cout << "Количество чисел: " << kek.size() << "," << '\t' << '\t' << "Количество чисел: " << pos.size() << "," << endl;
+    cout << "Максимальная цифра: " << itc_max_lst(kek) << "," << '\t' << '\t' << "Максимальная цифра: " << itc_max_lst(pos) <<  "," << endl;
+    cout << "Минимальная цифра: " << itc_min_lst(kek) << "," << '\t' << '\t' << "Минимальная цифра: " << itc_min_lst(pos) << "," << endl;
+    cout << "Сумма чисел: "<< itc_sumlst(kek) << "," << '\t'	<< '\t' << "Сумма чисел: "<< itc_sumlst(pos) <<"," << endl;
+    cout << "Среднее значение: "<< itc_sumlst(kek) / kek.size()<< '\t'	<< '\t' << "Среднее значение: " << itc_sumlst(pos) / pos.size() << endl;
+    cout << endl;
+    cout << "Количество нулей: " << null.size() << endl;
 }
