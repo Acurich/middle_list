d386e114bd944341cc55654b99f74ca757182fbd
Acurich <91753821+Acurich@users.noreply.github.com>
2022-01-20 12:26:58 +0300

    Update middle_list.h

--- middle_list.h
+++ middle_list.h
@@ -1,12 +1,13 @@
-#ifndef HOMEWORK_MIDDLE_LIST_H
-#define HOMEWORK_MIDDLE_LIST_H
+#ifndef EASY_LIST_H_INCLUDED
+#define EASY_LIST_H_INCLUDED
 
-#include <iostream>
-#include <vector>
+#include<vector>
+#include<iostream>
 using namespace std;
-int sizeofstr( string s );
-vector<char> itc_strtlist(string s);
-string itc_join(vector<char> list , string sep);
+
+int sizeOFSTR(string s);
+vector <char> itc_strtlist(string s);
+string itc_join(vector <char> list , string sep);
 string itc_rmstrspc(string s);
 string itc_rmstrchar(string s,string less);
 long itc_sumlst(const vector<int> &list);
@@ -16,4 +17,5 @@ void itc_odd_even_separator_lst(const vector<int> &list,vector<int> &list1,vecto
 void itc_pos_neg_separator_lst(const vector<int> &list,vector<int> &list1,vector<int> &list2,vector<int> &list3);
 void itc_odd_even_analysis_lst(const vector<int> &list);
 void itc_pos_neg_analysis_lst(const vector <int> &list);
-#endif //HOMEWORK_MIDDLE_LIST_H
+
+#endif // EASY_LIST_H_INCLUDED
